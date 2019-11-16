68a750e6637c0d63d54d0b174144693fea025bd7
Santos Joao <joaopaulo0503@gmail.com>
2019-11-16 11:00:58 +0100

    Setup firestore

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const admin = require('firebase-admin');
+
+let serviceAccount = require('./config/test-firebase-4521b-369c140244a5.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
